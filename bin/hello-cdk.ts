34550dd6a52f0bf4d6d106aa12d945a473b06ae9
Derric Gilling <derric@moesif.com>
2025-04-30 00:21:27 -0700

    Fix: Update Example to work with SBT 0.5.0 or greater
    This fix updates MoesifBilling to support breaking change that was introduced in SBT 0.5.0 via #91 due to IBilling changed to support IASyncFunction.

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { HelloCdkStack } from '../lib/hello-cdk-stack';
+import { ControlPlaneStack } from '../lib/control-plane';
+import { AppPlaneStack } from '../lib/app-plane';
 
 const app = new cdk.App();
-new HelloCdkStack(app, 'HelloCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const cp = new ControlPlaneStack(app, 'ControlPlaneStack', {});
+const ap = new AppPlaneStack(app, 'AppPlaneStack', {
+  eventManager: cp.eventManager,
 });
---
+++ lib/control-plane.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CognitoAuth, ControlPlane, IEventManager } from '@cdklabs/sbt-aws';
+import { Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {MoesifBilling, BillingProviderSlug} from '../../sbt-aws-moesif/lib'
+//import {MoesifBilling, BillingProviderSlug} from 'sbt-aws-moesif'
+
+export class ControlPlaneStack extends Stack {
+  public readonly regApiGatewayUrl: string;
+  public readonly eventManager: IEventManager;
+
+  constructor(scope: Construct, id: string, props: any) {
+    super(scope, id, props);
+
+    const cognitoAuth = new CognitoAuth(this, 'CognitoAuth', {
+      enableAdvancedSecurityMode: false, // only for testing purposes!
+      setAPIGWScopes: false, // only for testing purposes!
+    });
+
+    const moesifBilling = new MoesifBilling(stack, 'MoesifBilling', {
+      moesifApplicationId: '<<Your Moesif Application Id>>',
+      moesifManagementAPIKey: '<<Your Moesif Management API Key>>',
+      billingProviderSlug: BillingProviderSlug.STRIPE,
+      billingProviderSecretKey: '<<Your Billing Provider\'s Secret Such as for Stripe>>'
+    }
+   ); 
+
+    const controlPlane = new ControlPlane(this, 'ControlPlane', {
+      auth: cognitoAuth,
+      billing: moesifBilling,
+      systemAdminEmail: 'derric+2@moesif.com',
+    });
+    this.eventManager = controlPlane.eventManager;
+    this.regApiGatewayUrl = controlPlane.controlPlaneAPIGatewayUrl;
+  }
+}
+
+const app = new cdk.App();
+const cp = new ControlPlaneStack(app, 'ControlPlaneStack', {});
